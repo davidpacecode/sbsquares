5f445e7812aae03f1df135d4ae673412b5f96945
davidpacecode <davidpace@gmail.com>
2025-10-19 14:08:07 -0500

    starting turbo frames. put one around square_grid and it fixes jitters

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     member do
       patch :claim_squares
       patch :randomize_numbers
+      patch :update_scores
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
