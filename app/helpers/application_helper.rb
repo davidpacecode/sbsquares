efccc28ee621f7ca40f3ce0cb4dbb8daaf90d05f
davidpacecode <davidpace@gmail.com>
2026-01-01 22:57:05 -0600

    getting closer to done with games index view. show is next

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def sport_icon_tag(sport)
+        icon_name = case sport
+        when "nfl"
+          "football"
+        when "nba"
+          "basketball"
+        # ... etc
+        else
+          "trophy"
+        end
+        
+    icon_name
+  end
 end
+
