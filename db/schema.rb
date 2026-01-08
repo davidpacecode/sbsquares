c5dafa66ab117aa99dd27fe840b56cac5f59f61f
davidpacecode <davidpace@gmail.com>
2026-01-08 00:54:29 -0600

    i got like one more button working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_06_042505) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_08_044441) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -99,6 +99,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_06_042505) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email_address", null: false
+    t.string "nickname"
     t.string "password_digest", null: false
     t.integer "role", default: 0, null: false
     t.datetime "updated_at", null: false
